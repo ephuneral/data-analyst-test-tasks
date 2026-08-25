daa5458e0bede30d39f38bb90a5d0242877fb314
Evgenii <mail.evgeniiluzan@gmail.com>
2026-08-25 19:27:30 +0600

    task 2 done

---
+++ task2.sql
@@ -0,0 +1,22 @@
+-- 2 Задание. 
+-- дана таблица fin с заполненными полями month, emp_id, salary. 
+-- необходимо посчитать нарастающий итог по каждому сотруднику.
+-- результат отобразить в поле sum_salary
+-- fin: month, emp_id, salary, sum_salary
+-- 1, 1, 50, 50 	
+-- 2, 1, 50, 100 
+-- 3, 1, 50, 150
+-- 1, 2, 60, 60 
+-- 2, 2, 60, 120 
+-- 3, 2, 60, 180
+
+SELECT
+    month,
+    emp_id,
+    salary,
+    SUM(salary) OVER (
+        PARTITION BY emp_id
+        ORDER BY month        
+    ) AS sum_salary
+FROM fin
+ORDER BY emp_id, month;
